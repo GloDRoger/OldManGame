93b08bc33fd787aec265cd07be813ef2ce83935c
GloDRoger <414946261@qq.com>
2022-08-05 00:27:21 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue({
+      template: {
+        transformAssetUrls: {
+          video: ["src", "poster"],
+          source: ["src"],
+          img: ["src"],
+          image: ["xlink:href", "href"],
+          use: ["xlink:href", "href"],
+        },
+      },
+    }),
+  ],
+});
